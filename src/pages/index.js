1d28f42c33d708993a7d31ce602b53b800e3e113
Isaac Tait <isaac@mountaintopcoding.com>
2023-05-03 21:07:00 -0400

    feat: install prettier 🍟

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,15 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
+import Image from "next/image";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className='bg-blue-300 w-full flex justify-center rounded-xl'>
-        <h1 className='text-2xl font-black tracking-wider'>
-          Wormwood Saga
-        </h1>
+      <div className="bg-blue-300 w-full flex justify-center rounded-xl">
+        <h1 className="text-2xl font-black tracking-wider">Wormwood Saga</h1>
       </div>
     </main>
   );
