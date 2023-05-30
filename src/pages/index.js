7cde2285e0393cadebdd24a4e713f1935c5d03ec
Isaac Tait <isaac@mountaintopcoding.com>
2023-05-30 10:08:50 -0400

    feat: responsive layout 2.0 🥸

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,32 +5,32 @@ import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div className="heropattern-topography-yellow-400 bg-gray-100 pt-2 h-screen">
-      <header className="flex justify-between px-2 max-w-7xl mx-auto bg-slate-600 rounded-xl font-serif">
+    <div className="heropattern-topography-yellow-400 bg-gray-100 pt-2 h-screen overflow-y-scroll">
+      <header className="fixed top-0 w-full flex justify-between px-2 max-w-7xl mx-auto bg-slate-600 md:rounded-xl font-serif">
         <Link
           href="https://story.wormwoodsaga.com"
           target="__blank"
-          className="text-yellow-500 text-xs md:text-2xl font-semibold tracking-widest hover:text-white"
+          className="text-yellow-500 text-base text-center md:text-2xl font-semibold tracking-widest hover:text-white"
         >
           Read the Story
         </Link>
         <Link
-          href="https://www.buymeacoffee.com/Isaactait"
-          target="__blank"
-          className="text-yellow-500 text-xs md:text-2xl font-semibold tracking-widest hover:text-white"
-        >
-          Support the Author
-        </Link>
-        <Link
           href="/newsletter"
           target="__blank"
-          className="text-yellow-500 text-xs md:text-2xl font-semibold tracking-widest hover:text-white"
+          className="text-yellow-500 text-base text-center md:text-2xl font-semibold tracking-widest hover:text-white my-auto"
         >
           Newsletter
         </Link>
+        <Link
+          href="https://www.buymeacoffee.com/Isaactait"
+          target="__blank"
+          className="text-yellow-500 text-base text-center md:text-2xl font-semibold tracking-widest hover:text-white"
+        >
+          Support the Author
+        </Link>
       </header>
       <main className="flex flex-col items-center justify-between p-4">
-        <h1 className="font-semibold text-4xl text-center lg:text-6xl tracking-wider font-sans py-4 lg:py-8 xl:py-10 text-slate-600">
+        <h1 className="font-semibold text-4xl text-center lg:text-6xl tracking-wider font-sans py-4 lg:py-8 xl:py-10 text-slate-600 mt-4">
           Wormwood Saga
         </h1>
         {/* sm breakpoint image */}
@@ -49,21 +49,21 @@ export default function Home() {
           width={800}
           className="mx-auto rounded-xl hidden 2xl:block"
         />
-        <h3 className="text-sm md:text-lg lg:text-xl font-medium tracking-wide text-center p-4 md:p-6 lg:p-10 font-mono text-slate-600">
-          The saga is available to all. If you feel inclined you can{" "}
-          <Link
-            href="https://www.buymeacoffee.com/Isaactait"
-            target="__blank"
-            className="text-blue-400 hover:bg-slate-500 p-1 hover:text-white hover:rounded-xl"
-          >
-            buy me a coffee
-          </Link>
-          (so that I have the energy to keep working on publishing the saga)
+        <h3 className="text-sm md:text-lg lg:text-xl font-medium tracking-wide text-center p-4 md:p-6 lg:p-10 font-mono text-slate-700">
+          In a story as old as time Andor and Lilli - two young people whose
+          lives are &#34;normal&#34; - discover each other and fall in love.
+          However, as their love blossoms and grows a cosmic battle, that spans
+          the multiverse, destined to unleash cataclysmic destruction upends
+          their idyllic suburban &#34;American dream&#34;. Through the chaos
+          they find themselves thrust into a fight for survival against the
+          elements&nbsp;
+          <span className="font-bold">and</span> an ancient evil. As they
+          descend into the depths of lost hope, uncertainty, and a society
+          plunged into the dark ages will they find the strength to survive and
+          give the world a fighting chance for redemption?
         </h3>
       </main>
-      <div className="fixed bottom-0 w-full">
-        <Footer />
-      </div>
+      <Footer />
     </div>
   );
 }
